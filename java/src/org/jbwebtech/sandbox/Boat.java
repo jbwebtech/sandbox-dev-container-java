34b424302c777638ce2b570d3eea4e3125528d9a
jbwebtech <justinwbianchi@gmail.com>
2024-12-30 03:07:54 +0000

    Refactor to Java 8 compatability.

--- java/src/org/jbwebtech/sandbox/Boat.java
+++ java/src/org/jbwebtech/sandbox/Boat.java
@@ -1,5 +1,33 @@
-public record Boat(String name, int horsepower, double lengthInches) {
-    public Boat {
+public class Boat {
+    private final String name;
+    private final int horsepower;
+    private final double lengthInches;
+
+    public Boat(String name, int horsepower, double lengthInches) {
+        this.name = name;
+        this.horsepower = horsepower;
+        this.lengthInches = lengthInches;
         System.out.println("Created Boat: " + name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHorsepower() {
+        return horsepower;
+    }
+
+    public double getLengthInches() {
+        return lengthInches;
+    }
+
+    @Override
+    public String toString() {
+        return "Boat{" +
+                "name='" + name + '\'' +
+                ", horsepower=" + horsepower +
+                ", lengthInches=" + lengthInches +
+                '}';
     }
 }
--- java/src/org/jbwebtech/sandbox/Main.java
+++ java/src/org/jbwebtech/sandbox/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        final var b1 = new Boat("Lucky", 800, 240.781);
+        final Boat b1 = new Boat("Lucky", 800, 240.781);
         System.out.println(b1);
 
-        final var b2 = new Boat("Drifter", 1200, 319);
+        final Boat b2 = new Boat("Drifter", 1200, 319);
         System.out.println(b2);
 
-        final var b3 = new Boat("Lightning", 2000, 400);
+        final Boat b3 = new Boat("Lightning", 2000, 400);
         System.out.println(b3);
 
-        final var b4 = new Boat(b3.name(), b3.horsepower(), b3.lengthInches());
+        final Boat b4 = new Boat(b3.getName(), b3.getHorsepower(), b3.getLengthInches());
         System.out.println(b4);
-      }
+    }
 }
